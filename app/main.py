6e269a7c8742138f621b1d8f4a9014fd26b1ad30
Pedro Albani <pedroalbani@gmail.com>
2024-05-26 19:44:30 -0300

    Autenticação JWT

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.routes.importar_route import importar_route
 from app.routes.configuracao_route import configuracao_route
 from app.routes.comercio_route import comercio_route
 from app.routes.manufatura_route import manufatura_route
 from app.routes.infra_route import infra_route
+from app.routes.auth import router as auth_route, get_current_active_user 
+from app.backend.mongodb_connector import MongoConnector
 
 app = FastAPI(
     title="Embrapa Tech Challenge"
 )
 
-app.include_router(configuracao_route)
-app.include_router(importar_route)
-app.include_router(manufatura_route)
-app.include_router(comercio_route)
-app.include_router(infra_route)
+app.include_router(configuracao_route, dependencies=[Depends(get_current_active_user)])
+app.include_router(importar_route, dependencies=[Depends(get_current_active_user)])
+app.include_router(manufatura_route, dependencies=[Depends(get_current_active_user)])
+app.include_router(comercio_route, dependencies=[Depends(get_current_active_user)])
+app.include_router(infra_route, dependencies=[Depends(get_current_active_user)])
+app.include_router(auth_route, prefix="/auth") 
